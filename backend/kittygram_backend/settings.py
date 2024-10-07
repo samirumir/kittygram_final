20e575cdc8466370be4953aa591b2a7793dda5c4
Samir <148135908+samirumir@users.noreply.github.com>
2024-10-07 07:30:25 +0300

    Closed 1st review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,18 +2,19 @@
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv()
 
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '158.160.18.251',
-    'kittygramsamir.myftp.org'
-]
+DEBUG = ('TRUE' == os.getenv('DEBUG').upper)
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
